f563193614d0d8abee7ed5cfede485b4302e5eba
Atharv <110733438+atharvsankpal7@users.noreply.github.com>
2024-11-06 13:23:56 +0530

    .

--- src/app/api/students/route.ts
+++ src/app/api/students/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server'
 import { promises as fs } from 'fs'
 import path from 'path'
 
-const dataFilePath = path.join(process.cwd(), 'src/app/api/students/students.json')
+const dataFilePath = path.join(process.cwd(), './students.json')
 
 export async function GET() {
   try {
